0462321ecd4ae72a32d8d6fd768bb689cb5f24cb
Alexander M. Stolz <alexander.stolz@dvs.ag>
2022-06-05 11:45:26 +0200

    add publish_port

--- bridge.py
+++ bridge.py
@@ -1,34 +1,37 @@
+"""
+this is just a playground. use publish_port.py instead.
+"""
+
 # https://pyserial.readthedocs.io/en/latest/examples.html
 
 import socket
+import serial
 from types import SimpleNamespace as ns
 
 config = ns(
-    in_connections=ns(
-        tcp=ns(
-            port=50000,
-            max_connections=2,
-        ),
-        serial=ns(
-            port=50001,
-            baudrate=115200,
-        ),
+    in_connection=ns(
+        port=50000,
+        max_connections=1,
     ),
     out_connection=ns(
-        port=50002,
-        address='/dev/ttyUSB0',
-        baudrate=115200,
+        port='/dev/ttyUSB0',
+        baudrate=500000,
     ),
 )
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', config.tcp.port))
-s.listen(config.tcp.max_connections)
+s.bind(('', config.in_connection.port))
+s.listen(config.in_connection.max_connections)
 
 try:
     while True:
-        connection, client_ip = s.accept()
-        while True:
-            data = connection.recv(1024)
+        in_connection, client_ip = s.accept()
+        out_connection = serial.Serial(
+            config.out_connection.port, config.out_connection.baudrate
+        )
+        while ...:
+            data = in_connection.recv(1024)
+            out_connection.write(data)
+        out_connection.close()
 finally:
     s.close()
